ce0556dc5767eeea55d6e9e434bda8fba95608e4
hasan <hasan-siddiqui@outlook.com>
2018-05-20 15:31:56 -0700

    wired up redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+import Store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm/>
-        <hr/>
-        <Posts/>
-      </div>
+      <Provider store={Store}>
+        <div className="App">
+          <PostForm/>
+          <hr/>
+          <Posts/>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { createPost } from '../actions/postActions'
 
 class PostForm extends Component {
   constructor(props) {
@@ -29,15 +32,7 @@ class PostForm extends Component {
     }
     console.log("Test");
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    this.props.createPost(post);
   }
 
   render() {
@@ -61,4 +56,7 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+export default connect(null, { createPost })(PostForm);
